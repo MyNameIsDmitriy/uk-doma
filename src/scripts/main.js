dc3d06a9bc9989aa6787f703017825c3c9050532
MyNameIsDmitriy <gopanchuk.dm@mail.ru>
2025-03-16 00:00:31 +0300

    header adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,14 @@
+const burgerButton = document.querySelector('.header__burger-button');
+const burgerMenu = document.querySelector('.header__burger-menu');
+
+burgerButton.addEventListener('click', () => {
+  burgerMenu.classList.toggle('open');
+  burgerButton.classList.toggle('open');
+});
+
 new Swiper('.services__swiper', {
   slidesPerView: 3.5,
-  spaceBetween: 20, 
+  spaceBetween: 20,
+  loop: true,
+  grabCursor: true,
 });
